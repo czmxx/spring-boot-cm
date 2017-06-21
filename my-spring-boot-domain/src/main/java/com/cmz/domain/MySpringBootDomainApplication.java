7c9bb1fceb58fb817340ab465c1953d8a1854efa
CHENZHANMEI <chenzhanmei@huazhu.com>
2017-06-21 17:36:09 +0800

    添加实体类对象

--- my-spring-boot-domain/src/main/java/com/cmz/domain/MySpringBootDomainApplication.java
+++ my-spring-boot-domain/src/main/java/com/cmz/domain/MySpringBootDomainApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringBootDomainApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootDomainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootDomainApplication.class, args);
+    }
 }
