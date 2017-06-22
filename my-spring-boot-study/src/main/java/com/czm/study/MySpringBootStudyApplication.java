81f1690d8db11b170dcb74e191aadc546ff34ecc
CHENZHANMEI <chenzhanmei@huazhu.com>
2017-06-22 15:50:58 +0800

    添加学习项目

--- my-spring-boot-study/src/main/java/com/czm/study/MySpringBootStudyApplication.java
+++ my-spring-boot-study/src/main/java/com/czm/study/MySpringBootStudyApplication.java
@@ -1,13 +1,22 @@
-package com.czm.web;
+package com.czm.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class MySpringBootWebApplication extends SpringBootServletInitializer {
+public class MySpringBootStudyApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(MySpringBootWebApplication.class, args);
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+
+    private int st(int[] number) {
+
+        Arrays.sort(number);
+
+        return 0;
     }
 }
---
+++ my-spring-boot/README.md
@@ -0,0 +1,3 @@
+# Dynamic-data-sources
+使用Spring-boot  Mybatis druid 配置多数据源
+### 
