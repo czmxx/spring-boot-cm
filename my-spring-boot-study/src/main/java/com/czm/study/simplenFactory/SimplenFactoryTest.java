82d47be5c13db0233e34c26fefaf87fc0080e229
CHENZHANMEI <chenzhanmei@huazhu.com>
2017-06-23 17:46:06 +0800

    添加setting 文件

--- my-spring-boot-study/src/main/java/com/czm/study/simplenFactory/SimplenFactoryTest.java
+++ my-spring-boot-study/src/main/java/com/czm/study/simplenFactory/SimplenFactoryTest.java
@@ -7,17 +7,27 @@ public class SimplenFactoryTest {
 
     /**
      * 简单工厂模式
+     *
      * @param args
      */
 
     public static void main(String[] args) {
 
         //使用统一的接口去实现  使用是只需要调用具体的实体类就行
-        SendMessage sendEmail = new SendEmailMessage();
-        System.out.println(sendEmail.send());
+        send("sms");
+        send("email");
 
-        SendMessage sendSMS = new SendSmsMessage();
-        System.out.println(sendSMS.send());
+    }
+
+    public static void send(String type) {
+        if (type.equals("email")) {
+            SendMessage sendEmail = new SendEmailMessage();
+            System.out.println(sendEmail.send());
+        } else if ("sms".equals(type)) {
+            SendMessage sendSMS = new SendSmsMessage();
+            System.out.println(sendSMS.send());
+        }
+
 
     }
 
